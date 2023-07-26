15422153a80120deabab58f1e517c77991f9914c
lepzito <lucasrtinte19@gmail.com>
2023-07-26 14:42:21 -0300

    sprint2

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(PORT, () => console.log("Servidor correindo en el puerto " + PORT));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+);
+app.get("/productDetail", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "productDetail.html"))
+);
