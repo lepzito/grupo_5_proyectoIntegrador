43ec883eb7da50bd6113283b4dbcc2661493dce8
lepzito <lucasrtinte19@gmail.com>
2023-09-09 08:05:34 -0300

    validaciones primera parte

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const user = require("../models/user.js");
 const path = require("path");
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 const userControllers = {
   login: function (req, res) {
     res.render("./users/login");
@@ -45,13 +46,21 @@ const userControllers = {
     // }
     // );
     //}
+    const resultValidation = validationResult(req);
+
+    if (resultValidation.errors.length > 0) {
+      return res.render("./users/register", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
     let userToCreate = {
       ...req.body,
       password: bcrypt.hashSync(req.body.password, 8),
       imgUser: req.file.filename,
     };
     user.create(userToCreate);
-    return res.redirect("/login");
+    return res.redirect("./login");
   },
   profile: function (req, res) {
     res.render("./users/profile", { user: req.session.userLogged });
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/userControllers.js");
 const uploadUser = require("../middlewares/multerForUsers.js");
+const { body } = require("express-validator");
 //Aqui va el middleware de ruta que no permite ver algunas vistas si el usraio esta logeado
 const guestMiddleware = require("../middlewares/guestMiddleware.js");
 const authMiddleware = require("../middlewares/authMiddleware.js");
+
+//Validatios
+
+const validations = [
+  body("nombreUsuario").notEmpty().withMessage("Agrega un nombre"),
+  body("apellidoUsuario").notEmpty().withMessage("Agrega un apellido"),
+  body("email").notEmpty().withMessage("Agrega un email"),
+  body("password").notEmpty().withMessage("Agrega una contraseña"),
+];
 
 //Muestro la vista de login y registro
 router.get("/login", guestMiddleware, userControllers.login);
@@ -14,6 +24,7 @@ router.get("/register", guestMiddleware, userControllers.register);
 router.post(
   "/register",
   uploadUser.single("userImage"),
+  validations,
   userControllers.create
 );
 //Login envio
